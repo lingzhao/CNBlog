06297eba1a9bc87defc65550ae5c9dd6eb47bfe0
Sam <hui379190004@icloud.com>
2016-02-29 15:14:07 +0800

    Initial Commit

---
+++ CNBlog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CNBlog
+//
+//  Created by zzZgHhui on 16/2/29.
+//  Copyright © 2016年 zzZgHhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CNBlog/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CNBlog
+//
+//  Created by zzZgHhui on 16/2/29.
+//  Copyright © 2016年 zzZgHhui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
